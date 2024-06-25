d02eacebf70a7656e5bdbe1361b3b1c397bcbd3e
Ruben Flores <rfloresc92@gmail.com>
2024-06-25 01:43:39 -0400

    VercelChanges

--- resena_libros/resena_libros/resena_libros/settings.py
+++ resena_libros/resena_libros/resena_libros/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bf&msteq^dbv1q5=&pyxni%slypzr0%=12@b70@&nqrnfgss49
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
